7cf0c33111eb2d10471659192b7ac033c4221216
FreshSqueezedSquid <112971726+FreshSqueezedSquid@users.noreply.github.com>
2023-03-09 16:47:51 -0700

    refactored name of PageObject for better readability

---
+++ PageObjects/MainPage.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from PageObjects.LoginPage import LoginPage
+
+
+class MainPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    select_language = (By.CSS_SELECTOR, "#js-link-box-en")
+    login_button = (By.CSS_SELECTOR, "#pt-login-2")
+    star_icon = (By.CSS_SELECTOR, ".mw-ui-button.mw-ui-quiet.mw-ui-icon.mw-ui-icon-element.mw-ui-icon-star.mw-ui-icon-wikimedia-star.mw-ui-icon-small")
+
+    def getLanguage(self):
+        return self.driver.find_element(*MainPage.select_language)
+
+    def goToLogin(self):
+        self.driver.find_element(*MainPage.login_button).click()
+        loginPage = LoginPage(self.driver)
+        return loginPage
